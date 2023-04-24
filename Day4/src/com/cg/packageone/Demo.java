30a567240c11fced56eac50c97d36d60ec057984
divya <divyamurthi3579@gmail.com>
2023-04-24 17:24:05 +0530

    first commit

---
+++ Day4/src/com/cg/packageone/Demo.java
@@ -0,0 +1,11 @@
+package com.cg.packageone;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		new com.cg.packageone.Demo();
+		new com.cg.packageone.Main();
+
+	}
+
+}
---
+++ Day4/src/com/cg/packageone/Main.java
@@ -0,0 +1,11 @@
+package com.cg.packageone;
+
+public class Main {
+
+	public static void main(String[] args) {
+		com.cg.packageone.Main m = new com.cg.packageone.Main();
+		com.cg.packageone.Demo d= new com.cg.packageone.Demo();
+
+	}
+
+}
