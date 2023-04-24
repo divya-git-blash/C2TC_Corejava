3b8d201d892bb086dedc7f32020ff8ed462fdc5c
divya <divyamurthi3579@gmail.com>
2023-04-24 17:26:26 +0530

    first commit

---
+++ Day5/src/com/cg/packagetwo/TestFour.java
@@ -0,0 +1,14 @@
+package com.cg.packagetwo;
+
+import com.cg.packageone.TestOne;
+
+public class TestFour {
+
+	public static void main(String[] args) {
+		TestOne t1 = new TestOne();
+		System.out.println(t1.i);
+		t1.display();
+
+	}
+
+}
---
+++ Day5/src/com/cg/packagetwo/TestThree.java
@@ -0,0 +1,13 @@
+package com.cg.packagetwo;
+
+import com.cg.packageone.TestOne; //
+
+public class TestThree extends TestOne {
+
+	public static void main(String[] args) {
+		TestThree t3 = new TestThree();
+		System.out.println(t3.i);
+		t3.display();
+	}
+
+}
